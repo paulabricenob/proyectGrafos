b1a418bbdeaca7bc211dffe300d66644f68beb57
Nicola Colarusso <131668387+nicolacolarusso@users.noreply.github.com>
2023-10-24 19:42:09 -0400

    Ya con ui grafo instalada libreria

--- proyectoGrafos/src/proyectografos/ProyectoGrafos.java
+++ proyectoGrafos/src/proyectografos/ProyectoGrafos.java
@@ -4,6 +4,8 @@
  */
 package proyectografos;
 
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
 import GUI.Ventana1;
 import Objetos.Funciones;
 import Objetos.NodoPersona;
@@ -19,6 +21,18 @@ public class ProyectoGrafos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.setProperty("org.graphstream.ui", "swing");
+		
+		Graph graph = new SingleGraph("Prueba 1");
+
+		graph.addNode("A");
+		graph.addNode("B");
+		graph.addNode("C");
+		graph.addEdge("AB", "A", "B");
+		graph.addEdge("BC", "B", "C");
+		graph.addEdge("CA", "C", "A");
+
+		graph.display();
     }
     
 }
